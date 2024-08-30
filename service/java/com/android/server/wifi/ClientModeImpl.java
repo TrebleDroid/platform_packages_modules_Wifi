1772504fa069bc070c11407e1d596039647ed5cd
maheshkkv <maheshkkv@google.com>
2024-08-30 15:42:45 -0700

    Fix NPE when BSSID is null
    Bug: 361274708
    Flag: EXEMPT bugfix
    Test: atest com.android.server.wifi
    Change-Id: I454a207f994b49c341fc29f79d5d05d1b7d5c5d2

--- service/java/com/android/server/wifi/ClientModeImpl.java
+++ service/java/com/android/server/wifi/ClientModeImpl.java
@@ -5394,7 +5394,8 @@ public class ClientModeImpl extends StateMachine implements ClientMode {
      * @param bssid BSSID of the AP.
      * @return true if BSSID matches to one of the affiliated link BSSIDs, false otherwise.
      */
-    public boolean isAffiliatedLinkBssid(@NonNull MacAddress bssid) {
+    public boolean isAffiliatedLinkBssid(@Nullable MacAddress bssid) {
+        if (bssid == null) return false;
         List<MloLink> links = mWifiInfo.getAffiliatedMloLinks();
         for (MloLink link: links) {
             if (bssid.equals(link.getApMacAddress())) {
--- service/tests/wifitests/src/com/android/server/wifi/ClientModeImplTest.java
+++ service/tests/wifitests/src/com/android/server/wifi/ClientModeImplTest.java
@@ -10110,6 +10110,8 @@ public class ClientModeImplTest extends WifiBaseTest {
         assertTrue(mCmi.isAffiliatedLinkBssid(MacAddress.fromString(TEST_BSSID_STR)));
         assertTrue(mCmi.isAffiliatedLinkBssid(MacAddress.fromString(TEST_BSSID_STR1)));
         assertFalse(mCmi.isAffiliatedLinkBssid(MacAddress.fromString(TEST_BSSID_STR2)));
+        // Check for null BSSID
+        assertFalse(mCmi.isAffiliatedLinkBssid(null));
     }
 
     @Test
@@ -10137,6 +10139,8 @@ public class ClientModeImplTest extends WifiBaseTest {
         mLooper.dispatchAll();
         // Test isAffiliatedLinkBssid match fails with no NPE
         assertFalse(mCmi.isAffiliatedLinkBssid(MacAddress.fromString(TEST_BSSID_STR)));
+        // Check for null BSSID
+        assertFalse(mCmi.isAffiliatedLinkBssid(null));
     }
 
     /**
